64a479eba38a98db9656fb74ce3bc64c5a15f533
Logan Stone <logan.stone20@gmail.com>
2019-08-02 02:38:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,3 +25,20 @@ end
 def move(board, index, character)
   board[index] = character
 end
+
+def position_taken?(board, index)
+
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+def valid_move?(board, index)
+  if (!position_taken?(board, index) && index.between?(0,8))
+    true
+  else
+    false
+  end
+end
